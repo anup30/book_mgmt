d1044412f361975bd7688fdfc0e494c6c3134267
anup30 <anupbarua30@gmail.com>
2025-03-04 15:11:06 +0600

    update

--- booksapp/static/script.js
+++ booksapp/static/script.js
@@ -1,5 +1,6 @@
 // static js file
 window.onload = function() {
 	console.log("Page loaded!");
-	console.log("coded by Anup Barua. for assignment module 17, ostad. 4 march 2025");
+	console.log("coded by Anup Barua. for assignment module 17, ostad.");
+	console.log("date: 4 march 2025");
 };
